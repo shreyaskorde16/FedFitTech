17a9ac91b0fd43f3572d5526a55da62624d6e8bc
shreyaskorde16 <shreyaskorde16@gmail.com>
2025-07-30 14:37:04 +0200

    added files

--- FedFitTech/task.py
+++ FedFitTech/task.py
@@ -1,20 +1,17 @@
-"""new-flwr-wear: A Flower / PyTorch app."""
+"""Fedfittech task utils."""
 
 from collections import OrderedDict
 
 import torch
-import torch.nn as nn
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 
 
 def get_weights(net):
+    """Get the weights of the model."""
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
 
 def set_weights(net, parameters):
+    """Set the weights of the model."""
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
